60a2e5fb1c7a3808bb1fd79f2d1b5377aef44497
Walid Ghallab <wa.ghallab@gmail.com>
2023-12-31 02:10:02 +0100

    Update integration.test.js to not use suite.
    They seem to be causing flakiness. See https://github.com/walidghallab/video-experience-enhancer/pull/1

--- tests/integration.test.js
+++ tests/integration.test.js
@@ -1,6 +1,9 @@
 import { Options } from "selenium-webdriver/chrome.js";
 import { suite } from "selenium-webdriver/testing/index.js";
+import { Builder } from "selenium-webdriver/index.js";
 import { Browser, By, Key } from "selenium-webdriver";
+// import { Builder } from "selenium-webdriver/chromium.js";
+// import {ServiceBuilder} from "selenium-webdriver/chrome.js";
 import { expect } from "chai";
 import * as fs from "fs";
 
@@ -19,104 +22,103 @@ const RUN_IN_HEADLESS_MODE = true;
     More information here: https://www.selenium.dev/documentation/webdriver/waits/
 */
 
-suite(
-  function (env) {
-    let driver;
+describe("Video experience enhancer", function () {
+  let driver;
 
-    beforeEach(async function () {
-      const options = new Options();
-      options.addArguments(`load-extension=${EXTENSION_PATH}`);
-      if (RUN_IN_HEADLESS_MODE) {
-        options.addArguments("headless=new");
-      }
+  beforeEach(async function () {
+    const options = new Options();
+    options.addArguments(`load-extension=${EXTENSION_PATH}`);
+    if (RUN_IN_HEADLESS_MODE) {
+      options.addArguments("headless=new");
+    }
 
-      driver = await env.builder().setChromeOptions(options).build();
-    });
+    console.log("Starting the browser.");
+    driver = await new Builder()
+      .forBrowser("chrome")
+      .setChromeOptions(options)
+      .build();
+  });
 
-    it("popup renders correctly", async function () {
-      await driver.get(`chrome-extension://${EXTENSION_ID}/popup.html`);
-      const headerText = await driver.findElement(By.tagName("h1")).getText();
-      console.log(headerText);
-      expect(headerText).to.equal(
-        "Welcome to video experience enhancer"
-      );
-    });
+  it("popup renders correctly", async function () {
+    await driver.get(`chrome-extension://${EXTENSION_ID}/popup.html`);
+    const headerText = await driver.findElement(By.css("h1")).getText();
+    console.log(headerText);
+    expect(headerText).to.equal("Welcome to video experience enhancer");
+  });
 
-    it("video controls works correctly on coursera", async function () {
-      console.log("Opening Coursera page with a video.");
-      await driver.get(
-        `https://www.coursera.org/lecture/generative-ai-for-everyone/welcome-chD5R`
-      );
+  it("video controls works correctly on coursera", async function () {
+    console.log("Opening Coursera page with a video.");
+    await driver.get(
+      `https://www.coursera.org/lecture/generative-ai-for-everyone/welcome-chD5R`
+    );
 
-      if (!RUN_IN_HEADLESS_MODE) {
-        // Waiting for the cookies bar to load.
-        console.log("Accepting cookies.");
-        await sleep(1 * SECOND);
-        const cookiesButton = await driver
-          .findElement(By.css("#onetrust-accept-btn-handler"))
-          .click();
-      }
-
-      console.log("Loading and playing the video.");
-      await driver
-        .findElement(
-          By.css('button[data-track-component="click_play_video_button"]')
-        )
+    if (!RUN_IN_HEADLESS_MODE) {
+      console.log("Accepting cookies.");
+      // Waiting for the cookies bar to load.
+      await sleep(1 * SECOND);
+      const cookiesButton = await driver
+        .findElement(By.css("#onetrust-accept-btn-handler"))
         .click();
+    }
 
-      console.log("Entering fullscreen mode.");
-      const fullscreenButton = await driver
-        .findElement(By.css('button[title="Fullscreen"]'))
-        .click();
+    console.log("Loading and playing the video.");
+    await driver
+      .findElement(
+        By.css('button[data-track-component="click_play_video_button"]')
+      )
+      .click();
 
-      console.log("Testing that the video is playing.");
-      const video = await driver.findElement(By.css("video"));
-      let currentTime = Number(await video.getAttribute("currentTime"));
-      await sleep(0.1 * SECOND);
-      let newCurrentTime = Number(await video.getAttribute("currentTime"));
-      expect(newCurrentTime).to.be.greaterThan(currentTime);
+    console.log("Entering fullscreen mode.");
+    const fullscreenButton = await driver
+      .findElement(By.css('button[title="Fullscreen"]'))
+      .click();
 
-      console.log("Pausing the video.");
-      await driver.actions().sendKeys(" ").perform();
+    console.log("Testing that the video is playing.");
+    const video = await driver.findElement(By.css("video"));
+    let currentTime = Number(await video.getAttribute("currentTime"));
+    await sleep(0.1 * SECOND);
+    let newCurrentTime = Number(await video.getAttribute("currentTime"));
+    expect(newCurrentTime).to.be.greaterThan(currentTime);
 
-      console.log("Testing that the video is paused.");
-      currentTime = Number(await video.getAttribute("currentTime"));
-      await sleep(0.1 * SECOND);
-      newCurrentTime = Number(await video.getAttribute("currentTime"));
-      expect(currentTime).to.equal(newCurrentTime);
+    console.log("Pausing the video.");
+    await driver.actions().sendKeys(" ").perform();
 
-      console.log("Seeking forward.");
-      await driver.actions().sendKeys(Key.ARROW_RIGHT).perform();
+    console.log("Testing that the video is paused.");
+    currentTime = Number(await video.getAttribute("currentTime"));
+    await sleep(0.1 * SECOND);
+    newCurrentTime = Number(await video.getAttribute("currentTime"));
+    expect(currentTime).to.equal(newCurrentTime);
 
-      console.log("Testing that the video is seeked forward.");
-      newCurrentTime = Number(await video.getAttribute("currentTime"));
-      expect(newCurrentTime).to.be.closeTo(currentTime + 5, 5);
-      currentTime = newCurrentTime;
+    console.log("Seeking forward.");
+    await driver.actions().sendKeys(Key.ARROW_RIGHT).perform();
 
-      console.log("Seeking backward.");
-      await driver.actions().sendKeys(Key.ARROW_LEFT).perform();
+    console.log("Testing that the video is seeked forward.");
+    newCurrentTime = Number(await video.getAttribute("currentTime"));
+    expect(newCurrentTime).to.be.closeTo(currentTime + 5, 5);
+    currentTime = newCurrentTime;
 
-      console.log("Testing that the video is seeked backward.");
-      newCurrentTime = Number(await video.getAttribute("currentTime"));
-      expect(newCurrentTime).to.be.closeTo(currentTime - 5, 5);
-      currentTime = newCurrentTime;
+    console.log("Seeking backward.");
+    await driver.actions().sendKeys(Key.ARROW_LEFT).perform();
 
-      console.log("Playing the video.");
-      await driver.actions().sendKeys(" ").perform();
+    console.log("Testing that the video is seeked backward.");
+    newCurrentTime = Number(await video.getAttribute("currentTime"));
+    expect(newCurrentTime).to.be.closeTo(currentTime - 5, 5);
+    currentTime = newCurrentTime;
 
-      console.log("Testing that the video is playing.");
-      currentTime = Number(await video.getAttribute("currentTime"));
-      await sleep(0.1 * SECOND);
-      newCurrentTime = Number(await video.getAttribute("currentTime"));
-      expect(newCurrentTime).to.be.greaterThan(currentTime);
-    });
+    console.log("Playing the video.");
+    await driver.actions().sendKeys(" ").perform();
 
-    afterEach(async function () {
-      await driver.quit();
-    });
-  },
-  { browsers: [Browser.CHROME] }
-);
+    console.log("Testing that the video is playing.");
+    currentTime = Number(await video.getAttribute("currentTime"));
+    await sleep(0.1 * SECOND);
+    newCurrentTime = Number(await video.getAttribute("currentTime"));
+    expect(newCurrentTime).to.be.greaterThan(currentTime);
+  });
+
+  afterEach(async function () {
+    await driver.quit();
+  });
+});
 
 function sleep(ms) {
   return new Promise((r) => setTimeout(r, ms));
