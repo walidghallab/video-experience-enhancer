a56aaf0ef3038bd437b909c0f1720b81b9ce0f68
Walid Ghallab <wa.ghallab@gmail.com>
2024-01-07 23:24:54 +0100

    Refactor directories to make chrome directory minimal

--- popup/build-react.sh
+++ popup/build-react.sh
@@ -6,6 +6,7 @@ BACKUP_FILE="src/index.tsx.bak"
 cp $SOURCE_FILE $BACKUP_FILE
 sed -i 's/ mockValue={mockValue}//' $SOURCE_FILE
 
+export BUILD_PATH="../src/popup"
 react-scripts build
 
 cp $BACKUP_FILE $SOURCE_FILE
@@ -13,5 +14,5 @@ rm $BACKUP_FILE
 
 
 
-sed -i 's/src="\//src="/g' build/index.html
-sed -i 's/href="\//href="/g' build/index.html
+sed -i 's/src="\//src="/g' $BUILD_PATH/index.html
+sed -i 's/href="\//href="/g' $BUILD_PATH/index.html
