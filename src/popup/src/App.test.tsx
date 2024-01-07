de2a6454cfc107673857ea89abbd6760b4a5bf1d
Walid Ghallab <wa.ghallab@gmail.com>
2024-01-07 19:48:47 +0100

    Migrate the popup to React

--- src/popup/src/App.test.tsx
+++ src/popup/src/App.test.tsx
@@ -1,9 +1,77 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import ChromeContextProvider from "./ChromeContext";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("for supported websites", () => {
+  const mockValue = { url: "https://coursera.org/" };
+
+  it("renders welcome text", () => {
+    render(
+      <ChromeContextProvider mockValue={mockValue}>
+        <App />
+      </ChromeContextProvider>
+    );
+    const headerText = screen.getByText(
+      /Welcome to video experience enhancer/i
+    );
+    expect(headerText).toBeInTheDocument();
+  });
+
+  it("renders filing request text", async () => {
+    render(
+      <ChromeContextProvider mockValue={mockValue}>
+        <App />
+      </ChromeContextProvider>
+    );
+    await screen.findByText(
+      /If you have any new functionality that you want to see on this website/i
+    );
+  });
+
+  it("renders correct text for supported websites", async () => {
+    render(
+      <ChromeContextProvider mockValue={mockValue}>
+        <App />
+      </ChromeContextProvider>
+    );
+    await screen.findByText(/We currently support those keys/i);
+  });
+});
+
+describe("for unsupported websites", () => {
+  const mockValue = { url: "https://example.org/" };
+
+  it("renders welcome text", () => {
+    render(
+      <ChromeContextProvider mockValue={mockValue}>
+        <App />
+      </ChromeContextProvider>
+    );
+    const headerText = screen.getByText(
+      /Welcome to video experience enhancer/i
+    );
+    expect(headerText).toBeInTheDocument();
+  });
+
+  it("renders filing request text", async () => {
+    render(
+      <ChromeContextProvider mockValue={mockValue}>
+        <App />
+      </ChromeContextProvider>
+    );
+    await screen.findByText(
+      /If you have any new functionality that you want to see on this website/i
+    );
+  });
+  
+  it("renders correct text for unsupported websites", async () => {
+    render(
+      <ChromeContextProvider mockValue={mockValue}>
+        <App />
+      </ChromeContextProvider>
+    );
+    await screen.findByText(
+      /Functionality on this website already works out of the box/i
+    );
+  });
 });
