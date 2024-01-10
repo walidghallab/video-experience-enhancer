7c857c8d01b86904617f8e4eee0228f6f4eb6515
Walid Ghallab <wa.ghallab@gmail.com>
2024-01-10 01:21:29 +0100

    Add functionality for changing video playing rate by +/- 0.5s using keuboard shortcuts for all https and http urls

---
+++ src/all-websites.js
@@ -0,0 +1,47 @@
+(function () {
+  chrome.storage.sync.get(["disabled"], function ({ disabled }) {
+    if (!disabled) {
+      document.querySelector("body").addEventListener("keydown", handleKeyDown);
+    }
+    chrome.storage.onChanged.addListener(function (changes, namespace) {
+      if (changes.disabled) {
+        if (changes.disabled.newValue) {
+          document
+            .querySelector("body")
+            .removeEventListener("keydown", handleKeyDown);
+        } else {
+          document
+            .querySelector("body")
+            .addEventListener("keydown", handleKeyDown);
+        }
+      }
+    });
+  });
+
+  function handleKeyDown(e) {
+    const video = document.querySelector("video");
+    if (!video) {
+      return;
+    }
+
+    // Functionality for both fullscreen and non-fullscreen modes as long as the focused element is the video or an ancestor of the video.
+    if (
+      document.activeElement == video ||
+      document.activeElement.querySelector("video")
+    ) {
+      // Functionality with Ctrl key.
+      if (e.ctrlKey) {
+        switch (e.key) {
+          case "ArrowUp":
+            video.playbackRate += 0.5;
+            e.preventDefault();
+            break;
+          case "ArrowDown":
+            video.playbackRate -= 0.5;
+            e.preventDefault();
+            break;
+        }
+      }
+    }
+  }
+})();
