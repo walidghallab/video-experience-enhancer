7e7257027228247b2a0b2976df20fedc01872923
Walid Ghallab <wa.ghallab@gmail.com>
2024-01-15 18:43:23 +0100

    Add zip.extension to be used for uploading the extension to Chrome Web Store

---
+++ hack/zip-extension.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+# Args: $1: error message
+function print_error() {
+    echo >&2 -e "${RED}ERROR: $1${NC}"
+}
+
+# Args: $1: success message
+function print_success() {
+    echo >&2 -e "${GREEN}SUCCESS: $1${NC}"
+}
+
+if [[ ! $PWD == *hack ]]; then
+    print_error 'You must run this script from the hack directory'
+    exit 1
+fi
+
+ZIPPED_FILE_NAME="video-experience-enhancer.zip"
+
+cd .. && make && rm -f $ZIPPED_FILE_NAME && zip -r $ZIPPED_FILE_NAME src && mv $ZIPPED_FILE_NAME hack
+
+if [[ $? != "0" ]]; then
+    print_error "Failed to zip the extension"
+    exit 1
+fi
+
+print_success 'The extension has been zipped successfully'
