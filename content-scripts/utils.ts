2c8bf2bdd684b19655649c83aaf15fde53490ea7
Walid Ghallab <wa.ghallab@gmail.com>
2024-01-21 21:52:38 +0100

    Add functionality to add a snackbar (aka toast)

--- content-scripts/utils.ts
+++ content-scripts/utils.ts
@@ -77,3 +77,107 @@ export function downloadVideo(url: string, filename: string) {
     })
     .catch((err) => console.error(err));
 }
+
+// We need to make sure those constants are unique across all websites so they are randomly generated constants.
+export const SNACKBAR_ID = "snackbar-iqrvzpgvtse1s9mp19q8yyve5p2k2urk";
+export const SNACKBAR_SHOW_CLASS = `show-${SNACKBAR_ID}`;
+export const SNACKBAR_HIDE_CLASS = `hide-${SNACKBAR_ID}`;
+const FADE_IN_ANIMATION = `fadein-${SNACKBAR_ID}`;
+const FADE_OUT_ANIMATION = `fadeout-${SNACKBAR_ID}`;
+
+const SNACKBAR_PERIOD = 3000; // 3 seconds
+const SNACKBAR_SET_TIMEOUT_DATA_ATTRIBUTE = "data-snackbar-set-timeout"; // Used to clear setTimeout when the snackbar is shown again before the timeout is reached.
+
+export function insertSnackbar() {
+  if (document.getElementById(SNACKBAR_ID)) {
+    return;
+  }
+
+  const style = document.createElement("style");
+  style.innerHTML = `
+    #${SNACKBAR_ID} {
+      visibility: hidden;
+      min-width: 250px;
+      margin-left: -125px;
+      background-color: #333;
+      color: #fff;
+      text-align: center;
+      border-radius: 2px;
+      padding: 16px;
+      position: fixed;
+      z-index: 1;
+      left: 50%;
+      bottom: 30px;
+      font-size: 17px;
+    }
+    
+    #${SNACKBAR_ID}.${SNACKBAR_SHOW_CLASS} {
+      visibility: visible;
+      -webkit-animation: ${FADE_IN_ANIMATION} 0.5s;
+      animation: ${FADE_IN_ANIMATION} 0.5s;
+      animation-fill-mode: forwards;
+    }
+
+    #${SNACKBAR_ID}.${SNACKBAR_HIDE_CLASS} {
+      visibility: visible;
+      -webkit-animation: ${FADE_OUT_ANIMATION} 0.5s;
+      animation: ${FADE_OUT_ANIMATION} 0.5s;
+      animation-fill-mode: forwards;
+    }
+    
+    @-webkit-keyframes ${FADE_IN_ANIMATION} {
+      from {bottom: 0; opacity: 0;} 
+      to {bottom: 30px; opacity: 1;}
+    }
+    
+    @keyframes ${FADE_IN_ANIMATION} {
+      from {bottom: 0; opacity: 0;}
+      to {bottom: 30px; opacity: 1;}
+    }
+    
+    @-webkit-keyframes ${FADE_OUT_ANIMATION} {
+      from {bottom: 30px; opacity: 1;} 
+      to {bottom: 0; opacity: 0;}
+    }
+    
+    @keyframes ${FADE_OUT_ANIMATION} {
+      from {bottom: 30px; opacity: 1;}
+      to {bottom: 0; opacity: 0;}
+    }
+  `;
+
+  const wrapper = document.createElement("div");
+  wrapper.innerHTML = `
+    <div id="${SNACKBAR_ID}"></div>
+  `;
+  wrapper.appendChild(style);
+  document.body.appendChild(wrapper);
+}
+
+// showSnackbar shows a snackbar with the given message for {@link SNACKBAR_PERIOD}.
+// If the snackbar is called again before the timeout is finished, it will reset the timeout and update the message.
+export function showSnackbar(message: string) {
+  const snackbar = document.getElementById(SNACKBAR_ID);
+  if (!snackbar) {
+    console.error("Snackbar not found");
+    return;
+  }
+  snackbar.innerHTML = message;
+  if (snackbar.className.includes(SNACKBAR_SHOW_CLASS)) {
+    clearTimeout(
+      Number(snackbar.getAttribute(SNACKBAR_SET_TIMEOUT_DATA_ATTRIBUTE))
+    );
+  }
+  snackbar.className = SNACKBAR_SHOW_CLASS;
+  const setTimeoutVal = setTimeout(function () {
+    snackbar.className = snackbar.className.replace(
+      SNACKBAR_SHOW_CLASS,
+      SNACKBAR_HIDE_CLASS
+    );
+  }, SNACKBAR_PERIOD);
+
+  snackbar.setAttribute(
+    SNACKBAR_SET_TIMEOUT_DATA_ATTRIBUTE,
+    String(setTimeoutVal)
+  );
+}
